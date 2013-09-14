2eace02e05d1cdabf4eb275f81ba87f01b6af7e3
Timo Westkämper <timo.westkamper@mysema.com>
2013-09-15 00:52:55 +0300

    Add basic presets

--- src/rogue_presets/core.clj
+++ src/rogue_presets/core.clj
@@ -2,20 +2,53 @@
   (:require [rogue-presets.utils :refer :all]
             [rogue-presets.serializers :refer :all]))
 
- (defmacro defpreset
+(def defaults 
+  (let [text (slurp "resources/rogue.ttl")
+        keys (map second (re-seq #"lv2:symbol \"([\w_]+)\"" text))
+        values (map second (re-seq #"lv2:default ([\d\.]+)" text))]
+    (zipmap (drop 3 keys) values)))
+
+(defmacro defpreset
   [name label & contents]
   (list 'def name
         (list 'with-meta 
               (cons 'merge contents)
                {:name (str name) :label label})))
 
- ; basic leads pads basses fm reeds brasses strings effects
+ ; basic 
  
-(defpreset basic-saw 
-  "Basic Saw"
-  (bus_a :level 1)
-  (osc1 :type saw :level 1 :level_a 1))
+(defn basic-osc
+  [osc-type]
+  (merge defaults 
+         (bus_a :level 0.5)
+         (osc1 :type osc-type :level 1 :level_a 1)))
+
+(defpreset basic-saw "Basic Saw"
+  (basic-osc saw))
+
+(defpreset basic-pulse "Basic Pulse"
+  (basic-osc pulse))
+
+(defpreset basic-tri "Basic Tri"
+  (basic-osc tri))
+
+; phase distortion
+
+(defpreset pd-saw "PD Saw"
+  (basic-osc pd_saw))
+
+(defpreset pd-square "PD Square"
+  (basic-osc pd_square))
+
+(defpreset pd-pulse "PD Pulse"
+  (basic-osc pd_pulse))
+
+; leads pads basses fm percussion reeds brasses strings effects
 
 (comment
   (serialize basic-saw))         
+
+(comment
+  (dump "../rogue" basic-saw basic-pulse basic-tri pd-saw pd-square pd-pulse))
+
   
--- src/rogue_presets/serializers.clj
+++ src/rogue_presets/serializers.clj
@@ -17,6 +17,19 @@
   rdfs:label \"" label "\" ;
   lv2:port\n  "))
 
+(def presets-intro 
+  "@prefix lv2: <http://lv2plug.in/ns/lv2core#> .
+@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+@prefix pset: <http://lv2plug.in/ns/ext/presets#> .
+@prefix base: <http://www.github.com/timowest/>.")
+
+(defn preset
+  [symbol]
+  (str "<http://www.github.com/timowest/rogue#" symbol ">
+  a pset:Preset ;
+  lv2:appliesTo base:rogue ;
+  rdfs:seeAlso  <presets/" symbol ".ttl> ."))
+
 (defn- port
   [name value]
   (str "[ lv2:symbol \"" name "\" ; pset:value " value " ]"))
@@ -33,4 +46,15 @@
                               (port k v)))
       ".")))
                  
-  
+  (defn dump
+    [path & presets]
+    (let [presets-txt (str presets-intro
+                           "\n\n"
+                           (string/join "\n\n"
+                                        (map (comp preset :name meta) presets)))]
+      (spit (str path "/presets.ttl") presets-txt)
+      (doseq [preset presets]
+        (spit (str path "/presets/" (:name (meta preset)) ".ttl") 
+              (serialize preset)))))
+          
+    
