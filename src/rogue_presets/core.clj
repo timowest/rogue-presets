222c8e3bbbb74a451f13d2cabd92f21c1182b65d
Timo Westkämper <timo.westkamper@mysema.com>
2013-09-15 15:30:52 +0300

    Update presets

--- src/rogue_presets/core.clj
+++ src/rogue_presets/core.clj
@@ -63,7 +63,7 @@
          (osc3 :level 1 :level_b 0.25)
          (osc4 :level 1 :level_b 0.25)))
 
-(defpreset pulse-lead1 "Pulse Lead 1"
+(defpreset lead-pulse1 "Pulse Lead 1"
   two_oscs
   (osc1 :type pulse :width 0.25 :fine -0.05)
   (osc2 :type pulse :width 0.75 :fine 0.05)
@@ -73,16 +73,7 @@
   (modulations [mod_env2 mod_flt1_freq 0.6])
   (reverb-fx))
 
-(defpreset pulse-lead2 "Pulse Lead 2"
-  four-oscs
-  (osc1 :type pulse :width 0.4 :fine -0.2)
-  (osc2 :type pulse :width 0.5 :fine -0.1)
-  (osc3 :type pulse :width 0.6 :fine 0.1)
-  (osc4 :type pulse :width 0.7 :fine 0.2)
-  (bus_a :level 0.5)
-  (env1 :attack 0.03 :sustain 0.5 :release 1 :curve 0.7))
-
-(defpreset pulse-lead3 "Pulse Lead 3"
+(defpreset lead-pulse2 "Pulse Lead 2"
   four-oscs-ab
   (osc1 :type pulse :width 0.4 :fine -0.2)
   (osc2 :type pulse :width 0.5 :fine -0.1)
@@ -95,7 +86,9 @@
   (modulations [mod_env2 mod_flt1_freq 1]
                [mod_env2 mod_flt2_freq 1]))
 
-(defpreset saw-lead1 "Saw Lead 1"
+; stacked
+
+(defpreset stacked-saws "Stacked Saws"
   four-oscs
   (osc1 :type tri :width 0.95 :fine -0.2)
   (osc2 :type tri :width 0.96 :fine -0.1)
@@ -103,11 +96,38 @@
   (osc4 :type tri :width 0.98 :fine 0.2)
   (bus_a :level 0.5)
   (env1 :attack 0.03 :sustain 0.5 :release 1 :curve 0.7))
-    
-; pads basses fm percussion effects 
+
+(defpreset stacked-pulses "Stacked Pulses"
+  four-oscs
+  (osc1 :type pulse :width 0.4 :fine -0.2)
+  (osc2 :type pulse :width 0.5 :fine -0.1)
+  (osc3 :type pulse :width 0.6 :fine 0.1)
+  (osc4 :type pulse :width 0.7 :fine 0.2)
+  (bus_a :level 0.5)
+  (env1 :attack 0.03 :sustain 0.5 :release 1 :curve 0.7))
+  
+; pads
+
+(defpreset pad-arctic "Arctic Pad"
+  defaults
+  (osc1 :type fm1 :level 1)
+  (osc2 :type saw :level 1 :coarse 12 :out_mod 2 :level_a 1) ; RM modulated
+  (filter1 :type svf_lp :freq 220 :q 0.3 :level 1)
+  (env1 :attack 0.3 :sustain 0.6 :release 1.2 :curve 0.7)
+  (env2 :attack 1 :sustain 1 :release 1.22 :curve 0.7)
+  (lfo1 :type lfo_tri :freq 2)
+  (reverb-fx :bandwidth 0.75 :tail 1 :damping 0.25 :blend 0.75)
+  (modulations [mod_lfo1 mod_osc1_pitch 0.2]
+               [mod_lfo1 mod_flt1_freq 0.2]
+               [mod_lfo1 mod_osc2_amp 0.2]
+               [mod_env2 mod_flt1_freq 0.8]))
+
+; basses fm percussion effects 
 
 (defn -main
   []
   (dump "../rogue" 
         basic-saw basic-pulse basic-tri pd-saw pd-square pd-pulse
-        pulse-lead1 pulse-lead2 pulse-lead3 saw-lead1))
+        lead-pulse1 lead-pulse2
+        stacked-saws stacked-pulses
+        pad-arctic))
