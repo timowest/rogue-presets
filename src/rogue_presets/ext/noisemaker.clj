1993e911225e4cb92882b98d7a255f928ebb2024
Timo Westkämper <timo.westkamper@mysema.com>
2013-12-20 22:04:26 +0200

    Improve conversion

--- src/rogue_presets/ext/noisemaker.clj
+++ src/rogue_presets/ext/noisemaker.clj
@@ -1,10 +1,13 @@
 (ns rogue-presets.ext.noisemaker
   (:require [rogue-presets.utils :refer :all]))
 
+(def SR 44100.0)
+
 (def filter-type
   (comp {1 lp_24db 2 lp_18db 3 lp_12db 4 lp_6db
          5 hp_24db 6 bp_24db 7 n_24db} 
-        long (partial * 7.000001)))  
+        long 
+        (partial * 7.000001)))  
 
 (defn osc1-waveform
   [v]
@@ -25,20 +28,20 @@
         (partial * 5.000001))) 
 
 (def lfo1-destination
-  (comp {0 mod_no_target 1 mod_no_target 2 mod_flt1_freq 3 mod_osc1_pitch 
-         4 mod_osc2_pitch 5 mod_osc1_pwm 6 mod_osc1_mod 7 mod_lfo2_sp} 
+  (comp {0 mod_no_target 1 mod_flt1_freq 2 mod_osc1_pitch 
+         3 mod_osc2_pitch 4 mod_osc1_pwm 5 mod_osc1_mod 6 mod_lfo2_sp} 
         long
         (partial * 7.000001)))
 
 (def lfo2-destination
-  (comp {0 mod_no_target 1 mod_no_target 2 mod_flt1_freq 3 mod_osc1_pitch 
-         4 mod_osc2_pitch 5 mod_bus_a_pan 6 mod_volume 7 mod_lfo1_sp} 
+  (comp {0 mod_no_target 1 mod_flt1_freq 2 mod_osc1_pitch 
+         3 mod_osc2_pitch 4 mod_bus_a_pan 5 mod_volume 6 mod_lfo1_sp} 
         long 
         (partial * 7.000001)))
 
 (def freead-destination
-  (comp {0 mod_no_target 1 mod_no_target 2 mod_flt1_freq 3 mod_osc1_pitch 
-         4 mod_osc2_pitch 5 mod_osc1_pwm 6 mod_osc1_mod} 
+  (comp {0 mod_no_target 1 mod_flt1_freq 2 mod_osc1_pitch 
+         3 mod_osc2_pitch 4 mod_osc1_pwm 5 mod_osc1_mod} 
         long 
         (partial * 6.000001)))
 
@@ -49,11 +52,20 @@
 
 (defn log-scaled-value 
   [v]
-  (/ (- (Math/exp (- v (Math/log 20.0))) 1.0) 19.0))
+  (/ (- (Math/exp (* v (Math/log 20.0))) 1.0) 19.0))
 
 (defn log-scaled-value-filter
   [v]
-  (/ (- (Math/exp (- v (Math/log 8.0))) 1.0) 7.0))
+  (/ (- (Math/exp (* v (Math/log 8.0))) 1.0) 7.0))
+
+(defn log-scaled-value-centered
+  [v]
+  (let [v (* (- v 0.5) 2.0)]
+    (* v v v)))
+
+(defn filter-cutoff
+  [v]
+  (* (log-scaled-value-filter v) SR 0.5))
 
 (defn log-scaled-volume
   [v max]
@@ -78,6 +90,12 @@
 (defn osc-finetune
   [v]
   (* (- v 0.5) 2.0))
+
+(defn sustain
+  [v]
+  (log-scaled-volume v 1.0))
+
+(def mod-amount log-scaled-value-centered)
   
 (comment 
   ; modulations
@@ -89,12 +107,15 @@
 
 (defn transform
   [m] ; map of keyword to value
+  (into {} (map (fn [[k v]] [(name k) v])
   {; programname
    :volume       (volume (:volume m))
    
    ; filter
+   :filter1_on   true
+   :filter1_level 1.0
    :filter1_type (filter-type (:filtertype m))
-   :filter1_freq (log-scaled-value-filter (:cutoff m))
+   :filter1_freq (filter-cutoff (:cutoff m)) 
    :filter1_q    (:resonance m)
    
    ; osc
@@ -103,7 +124,8 @@
    
    ; osc1
    :osc1_on      (> (:osc1volume m) 0.0)
-   :osc1_level   (osc-volume (:osc1volume m))
+   :osc1_level_a (> (:osc1volume m) 0.0)
+   :osc1_level   (osc-volume (:osc1volume m)) 
    :osc1_type    (osc1-waveform (:osc1waveform m))
    :osc1_coarse  (+ (osc-tune (:osc1tune m)) 
                     (transpose (:transpose m)))
@@ -113,7 +135,8 @@
 
    ; osc2
    :osc2_on      (> (:osc2volume m) 0.0) 
-   :osc2_level   (osc-volume (:osc2volume m))
+   :osc2_level_a (> (:osc2volume m) 0.0) 
+   :osc2_level   (osc-volume (:osc2volume m)) 
    :osc2_type    (osc2-waveform (:osc2waveform m))
    :osc2_coarse  (+ (osc-tune (:osc2tune m))
                     (transpose (:transpose m)))
@@ -123,26 +146,27 @@
    
    ; osc3
    :osc3_on      (> (:osc3volume m) 0.0)
-   :osc3_level   (osc-volume (:osc3volume m))
+   :osc3_level_a (> (:osc3volume m) 0.0)
+   :osc3_level   (osc-volume (:osc3volume m)) 
    
    ; envs
    
    ; env1
    :env1_attack  (:ampattack m)
    :env1_decay   (:ampdecay m)
-   :env1_sustain (:ampsustain m)
+   :env1_sustain (sustain (:ampsustain m))
    :env1_release (:amprelease m)
    
    ; env2
-   ; filtercontour TODO
+   :env2_on      (not= (:filtercontour m) 0.5)
    :env2_attack  (:filterattack m)
    :env2_decay   (:filterdecay m)
-   :env2_sustain (:filtersustain m)
+   :env2_sustain (sustain (:filtersustain m))
    :env2_release (:filterrelease m)
    
    :mod2_src     mod_env2
    :mod2_target  mod_flt1_freq
-   :mod2_amount  1 ; TODO
+   :mod2_amount  (mod-amount (:filtercontour m))
    
    ; env3
    :env3_on      (> (:freeadamount m) 0.0)
@@ -151,7 +175,7 @@
    
    :mod3_src     mod_env3
    :mod3_target  (freead-destination (:freeaddestination m)) 
-   :mod3_amount  (:freeadamount m)
+   :mod3_amount  (mod-amount (:freeadamount m))
    
    ; lfos
    
@@ -165,19 +189,19 @@
    
    :mod4_src      mod_lfo1
    :mod4_target  (lfo1-destination (:lfo1destination m))
-   :mod4_amount  (:lfo1amount m)
+   :mod4_amount  (mod-amount (:lfo1amount m))
    
    ; lfo2
    :lfo2_on       (> (:lfo2amount m) 0.0)
    :lfo2_type    (lfo-waveform (:lfo2waveform m))
-   :lfo2_rate    (log-scaled-rate (:lfo2rate m))
+   :lfo2_freq    (log-scaled-rate (:lfo2rate m))
    :lfo2_start   (:lfo2phase m)
    ; lfo2sync
    ; lfo2keytrigger
    
    :mod5_src     mod_lfo2
    :mod5_target  (lfo2-destination (:lfo2destination m))
-   :mod5_amount  (:lfo2amount m) 
+   :mod5_amount  (mod-amount (:lfo2amount m)) 
          
    ; velocity
   
@@ -237,6 +261,6 @@
    ; tab2open 
    ; tab3open 
    ; tab4open
-  })
+  })))
 
 ; TODO function to read noisemaker xml preset into clojure map
